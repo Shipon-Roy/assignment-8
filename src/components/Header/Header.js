0be80e88fc44f759d50ab6e494db7c5c119db9e8
Shpon Roy <shiponroy509@gmail.com>
2021-09-25 23:41:32 +0600

    react commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="text-center bg-info">
+            <h1>Super Hacking Movie</h1>
+        </div>
+    );
+};
+
+export default Header;
